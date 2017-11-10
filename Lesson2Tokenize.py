77fdcb24f4dfa7964492ec683e6ab6a8c09ad9e6
Danh Nguyen <danhnguyen267@gmail.com>
2017-11-09 23:20:30 -0800

    More manual preprocessing before tokenizing

--- Lesson2Tokenize.py
+++ Lesson2Tokenize.py
@@ -1,18 +1,35 @@
 import nltk
+from string import punctuation
 from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
 #load data
 filename = 'ALittleJourney.txt'
 file = open(filename, 'rt', encoding='utf-8')
 file_text = file.read()
 file.close()
+###########################
+#some manual preprocessing#
+###########################
 
-#some manual preprocessing
-# words = file_text.split()
-# tokens = word_tokenize([word.lower() for word in words])
+#split into words by white space
+words = file_text.split()
 
-tokens = word_tokenize(file_text)
+#convert to lower case
+words = [word.lower() for word in words]
+
+#remove punctuation from each word
+table = str.maketrans('', '', punctuation)
+stripped = [w.translate(table) for w in words]
+
+#remove stop words
+stop_words = set(stopwords.words('english'))
+words = [w for w in words if not w in stop_words]
+
+#tokenize the list of words now converted to a string
+tokens = word_tokenize(''.join(words))
 
 #save tokenized text as new file
-with open("ALittleJourneyTokenized.txt", "w") as output:
-    output.write(str(tokens))
+tokenized_file = open('ALittleJourneyTokenized.txt', 'a')
+tokenized_file.write(str(tokens))
+tokenized_file.close()
